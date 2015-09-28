722dc722dae81bf5bfd6ed1df3f648d312e3a64f
wangyunxiang <wwyunxiang@sohu.com>
2015-09-29 00:36:56 +0800

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 src_dir=$(pwd)
 installed_dir=~/.vim/bundle
 
-for i in `cat $src_dir/install.sh |grep ^git|awk -F'/' '{print $5}'|cut -d'.' -f1`
+for i in `cat $src_dir/install.sh |grep ^git|awk -F'/' '{print $5}'|awk -F'.git' '{print $1}'`
 do 
 	if [ -d $installed_dir/$i ]
 	then
